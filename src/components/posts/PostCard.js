df6538174386ffa5499a21d2a9c947dfe1c11de4
DK <jenna_kim1@hotmail.com>
2020-10-01 04:33:01 +0200

    author info on postCard and postDetails

--- src/components/posts/PostCard.js
+++ src/components/posts/PostCard.js
@@ -5,24 +5,13 @@ const PostCard = ({ post }) => {
   return (
     <div className="max-w-sm rounded mb-2 overflow-hidden shadow-lg">
       <Link to={`/post/${post.id}`}>
-        <img
-          className="w-full"
-          src="https://tailwindcss.com/img/card-top.jpg"
-          alt="Sunset in the mountains"
-        />
-        <div className="px-6 py-4">
+        <div className="px-6 py-4 bg-green-500">
           <div className="font-bold text-xl mb-2">{post.title}</div>
-          <p className="text-gray-700 text-base">{post.content}</p>
         </div>
         <div className="px-6 pt-4 pb-2">
+          <p className="text-gray-700 text-base">{post.content}</p>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #photography
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #travel
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #winter
+            {post.authorFirstName} {post.authorLastName}
           </span>
         </div>
       </Link>
--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -1,12 +1,15 @@
 export const createPost = (post) => {
   return (dispatch, getState, { getFirestore, getFirebase }) => {
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
     firestore
       .collection("posts")
       .add({
         ...post,
-        // authorId,
-        // attachmentUrl,
+        authorId: authorId,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
         createdAt: Date.now(),
       })
       .then(() => {
